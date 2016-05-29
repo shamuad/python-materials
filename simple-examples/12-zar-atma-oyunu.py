33e43567359f463f75e740258e87ee497d048f46
shamuad <dinarari@gmail.com>
2016-05-29 19:44:14 +0300

    change the code zar atma oyunu

--- simple-examples/12-zar-atma-oyunu.py
+++ simple-examples/12-zar-atma-oyunu.py
@@ -1,74 +1,37 @@
 import random
 
-raw_input("Please user 1 press ENTER for the dices") 
-user1_dice1 = random.randint(1 , 6)      
-user1_dice2 = random.randint(1 , 6)
-print "User One  dice {} - {}".format(user1_dice1 , user1_dice2)
+raw_input("Please user 1 press ENTER for the dices")
+user1_dice1 = random.randint(1, 6)
+user1_dice2 = random.randint(1, 6)
 
-raw_input("Please user 2 press ENTER for the dices")           
-user2_dice1 = random.randint(1 , 6)
-user2_dice2 = random.randint(1 , 6)
-print "User Two  dice {} - {}".format(user2_dice1 , user2_dice2)    
+user1_get_double = False
+if user1_dice1 == user1_dice2:
+    user1_get_double = True
 
-if user1_dice1 == user1_dice2 :
-    user1_same = user1_dice1 + user1_dice2
-else :
-    user1_same = 0
-    
-if user2_dice1 == user2_dice2 :
-    user2_same = user2_dice1 + user2_dice2
-else :
-    user2_same = 0
-    
-print "user1_same : {} , user2_same : {}".format(user1_same , user2_same)
+print "User One  dice {} - {}".format(user1_dice1, user1_dice2)
 
-'''    
-if user1_same <> 0 and user2_same <> 0 and user1_same > user2_same :
-    print "The winner is User One"
-elif user1_same <> 0 and user2_same <> 0 and user2_same > user1_same :
-    print "The winner is User Two"
-elif user1_same <> 0 and user2_same <> 0 and user2_same == user1_same :
-    print "Draw - No winner - woow same dice"
-elif user1_same <> 0 and user2_same == 0 :
-     print "The winner is User One"
-elif user2_same <> 0 and user2_same == 0 :
-    print "The winner is User Two"
-elif user1_same == 0 and user2_same == 0 and user1_dice1 + user1_dice2 > user2_dice1 + user2_dice2 :
-    print "The winner is User One"
-elif user1_same == 0 and user2_same == 0 and user1_dice1 + user1_dice2 < user2_dice1 + user2_dice2 :
-    print "The winner is User Two"
-else :
-    print "No winner" 
-'''
+raw_input("Please user 2 press ENTER for the dices")
+user2_dice1 = random.randint(1, 6)
+user2_dice2 = random.randint(1, 6)
 
-if user1_same <> 0 and user2_same <> 0 and user1_same > user2_same :
-    who_is_the_winner = "One"
-elif user1_same <> 0 and user2_same <> 0 and user2_same > user1_same :
-    who_is_the_winner = "Two"
-elif user1_same <> 0 and user2_same <> 0 and user2_same == user1_same :
-    who_is_the_winner = "Draw - No winner - woow same dice"
-elif user1_same <> 0 and user2_same == 0 :
-     who_is_the_winner = "One"
-elif user2_same <> 0 and user1_same == 0 :
-    who_is_the_winner = "Two"
-elif user1_same == 0 and user2_same == 0 and user1_dice1 + user1_dice2 > user2_dice1 + user2_dice2 :
-    who_is_the_winner = "One"
-elif user1_same == 0 and user2_same == 0 and user1_dice1 + user1_dice2 < user2_dice1 + user2_dice2 :
-    who_is_the_winner = "Two"
-else :
-    who_is_the_winner = "No winner"
-    
+user2_get_double = False
+if user2_dice1 == user1_dice2:
+    user2_get_double = True
+
+print "User Two  dice {} - {}".format(user2_dice1, user2_dice2)
+
+point_of_user1 = user1_dice1 + user1_dice2
+point_of_user2 = user2_dice1 + user2_dice2
+
+winner = 0
+if user1_get_double and not user2_get_double:
+    winner = 1
+elif not user1_get_double and user2_get_double:
+    winner = 2
+else:
+    winner = 1 if point_of_user1 > point_of_user2 else 2
 
 print "\n**************************"
 print "   The Winner is         "
-print "     {}                  ".format(who_is_the_winner)
+print "     {}                  ".format(winner)
 print "\n**************************"
- 
-    
-    
-    
-    
-
-
-
-
