bd1c3b31d0f561f7f55d7a4f4add620622687c42
shamuad <dinarari@gmail.com>
2016-05-31 12:25:06 +0300

    add calculater program

---
+++ simple-examples/21-calculate-area.py
@@ -0,0 +1,55 @@
+import math
+
+# Function calculates triangle area and return the result
+def triangle_area(base,height) :
+    return base * height / 2
+   
+# Function calculates rectangle area and return the result
+def rectangle_area(length,width) :
+    return length * width
+    
+# Function calculates Square area and return the result
+def square_area(side_lenght) :
+    return side_lenght * side_lenght
+    
+# Funtion calculates circle area and return the result
+def circle_area(radius) :
+    return math.pi * radius ** 2    
+
+ifchosed = True
+while ifchosed == True :
+    choice = int(raw_input("\n1) Triangle \n2) Rectangle \n3) Square \n4) Circle \n5) Quit \n\nwhich shape ? "))
+    if type(choice) != int :
+         print "You have to give numeric number"
+         continue
+    else :      
+        if  choice == 1 :
+            base = int(raw_input("Enter the base of triangle : "))
+            height = int(raw_input("Enter the height of triangle : "))
+            result = triangle_area(base,height)
+            print result
+            continue
+        elif choice == 2 :
+            lenght = int(raw_input("Enter the length of rectangle : "))
+            width = int(raw_input("Enter the width of rectangle : "))
+            result = rectangle_area(lenght,width)
+            print result
+            continue
+        elif choice == 3 :
+            side_lenght = int(raw_input("Enter the side lenght of the square : "))
+            result = square_area(side_lenght)
+            print result
+            continue
+        elif choice == 4 :
+            radius = int(raw_input("Enter the radius of the circle : "))
+            result = circle_area(radius)
+            print result
+            continue
+        else :
+            ifchosed = False
+            break     
+
+    
+ 
+   
+
