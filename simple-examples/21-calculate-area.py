8075229734ca9ae3568d8fed8f3035d837e02df0
shamuad <dinarari@gmail.com>
2016-06-01 09:24:45 +0300

    erase continue from the calculate code

--- simple-examples/21-calculate-area.py
+++ simple-examples/21-calculate-area.py
@@ -16,13 +16,11 @@ def square_area(side_lenght) :
 def circle_area(radius) :
     return math.pi * radius ** 2
     
-      
 ifchosed = True
 while ifchosed == True :
     choice = raw_input("\n1) Triangle \n2) Rectangle \n3) Square \n4) Circle \n5) Quit \n\nwhich shape ? ")    
     if choice.isdigit() == False :
          print "You have to give numeric number"
-         continue
     else :  
         choice = int(choice)    
         if  choice == 1 :
@@ -30,23 +28,19 @@ while ifchosed == True :
             height = int(raw_input("Enter the height of triangle : "))
             result = triangle_area(base,height)
             print result
-            continue
         elif choice == 2 :
             lenght = int(raw_input("Enter the length of rectangle : "))
             width = int(raw_input("Enter the width of rectangle : "))
             result = rectangle_area(lenght,width)
             print result
-            continue
         elif choice == 3 :
             side_lenght = int(raw_input("Enter the side lenght of the square : "))
             result = square_area(side_lenght)
             print result
-            continue
         elif choice == 4 :
             radius = int(raw_input("Enter the radius of the circle : "))
             result = circle_area(radius)
             print result
-            continue
         else :
             ifchosed = False
             break     
